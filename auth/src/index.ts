953c53eed86c1425d3fe555436308dd5622b1458
AqibJavaid899 <aqibjaved0910@gmail.com>
2023-12-25 01:53:16 +0500

    feature: add ingress nginx controller rules with a route in auth service and also implement the skaffold config for auto deployment and monitoring of k8s objects

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -5,7 +5,12 @@ import { json } from 'body-parser'
 const app = express();
 app.use(json());
 
+app.get('/api/users/currentuser', (req, res) => {
+    console.log("Hitting the correct URL")
+    res.json({ message: "api is working fine!" })
+})
+
 const port = 3000;
 app.listen(port, () => {
-    console.log(`Listening on Port ${port}!!`)
+    console.log(`Listening on Port ${port}!`)
 })
