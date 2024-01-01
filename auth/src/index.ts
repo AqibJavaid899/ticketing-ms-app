402fc721c1be70dc23c99e29c509dd56912478ad
AqibJavaid899 <aqibjaved0910@gmail.com>
2024-01-02 02:19:46 +0500

    feature: integrate the new custom error class and also change the express error handling mechanism to make async route handler also work with current implementation

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -1,24 +1,29 @@
 import express from "express";
-import { json } from 'body-parser'
+import { json } from "body-parser";
+import "express-async-errors";
 
 import { signinRouter } from "./routes/signin";
 import { signupRouter } from "./routes/signup";
 import { signoutRouter } from "./routes/signout";
 import { currentuserRouter } from "./routes/current-user";
 import { errorHandler } from "./middlewares/error-handler";
-
+import { NotFoundError } from "./errors/not-found-error";
 
 const app = express();
 app.use(json());
 
-app.use(currentuserRouter)
-app.use(signinRouter)
-app.use(signoutRouter)
-app.use(signupRouter)
+app.use(currentuserRouter);
+app.use(signinRouter);
+app.use(signoutRouter);
+app.use(signupRouter);
 
-app.use(errorHandler)
+app.all("*", async (req, res) => {
+  throw new NotFoundError();
+});
+
+app.use(errorHandler);
 
 const port = 3000;
 app.listen(port, () => {
-    console.log(`Listening on Port ${port}!`)
-})
+  console.log(`Listening on Port ${port}!`);
+});
